02193cdfcbfc2659511a11076ceda71bcefc9966
rubankumarsankar <rubankumarsankar@users.noreply.github.com>
2025-10-24 15:49:02 +0530

    update 1

--- study_mgmt/settings.py
+++ study_mgmt/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'study_db',
         'USER': 'root',
-        'PASSWORD': 'ruban',
+        'PASSWORD': 'ayati',
         'HOST': 'localhost',
         'PORT': '3306',
     }
